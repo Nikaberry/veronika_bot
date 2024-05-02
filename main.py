1c39b1c5c57cec5fcfc67cae2d5c8b5873395a9e
Nikaberry <veronikasmolkova@yandex.ru>
2024-05-02 22:30:00 +0300

    all

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from telebot import types
 from my_token import bot
 from russia_cities import cities
+from country_game import country
+
 
 used_words = []
-max_word_score = 0
-max_city_score = 0
 word_score = 0
 city_score = 0
+country_score = 0
 current_game = None
+
 
 with open("max_result_words.txt", "r") as f:
     max_word_score = int(f.read())
 with open("max_result_cities.txt", "r") as f:
     max_city_score = int(f.read())
+with open('max_result_countries.txt', 'r') as f:
+    max_country_score = int(f.read())
 
 
 @bot.message_handler(commands=["start"])
@@ -40,13 +44,14 @@ def show_rules(call):
                   "Этот бот не является <вторым игроком>, в бот нужно подряд вводить слова, "
                   "а бот будет проверять было ли уже сыграно это слово, напоминает на какую букву нужно написать "
                   "следующее слово. Если вы играете в русские города, то бот ещё проверяет есть ли такой город в "
-                  "России.")
+                  "России. Если вы играете в страны, то бот дополнительно проверяет"
+                  "есть ли такая страна.")
     bot.send_message(call.message.chat.id, rules_text, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'back')
 def back_to_menu(call):
-    global max_word_score, max_city_score, word_score, city_score
+    global max_word_score, max_city_score, word_score, city_score, max_country_score
     if word_score > max_word_score:
         max_word_score = word_score
         with open("max_result_words.txt", "w") as file:
@@ -55,11 +60,17 @@ def back_to_menu(call):
         max_city_score = city_score
         with open("max_result_cities.txt", "w") as file:
             file.write(str(max_city_score))
+    if country_score > max_country_score:
+        max_country_score = country_score
+        with open("max_result_countries.txt", "w") as file:
+            file.write(str(max_country_score))
 
     markup = types.InlineKeyboardMarkup(row_width=1)
     bot.send_message(call.message.chat.id, f"Максимальный результат в игре в слова: {max_word_score}",
                      reply_markup=markup)
     bot.send_message(call.message.chat.id, f"Максимальный результат в игре в города: {max_city_score}",
+                     reply_markup=markup)
+    bot.send_message(call.message.chat.id, f"Максимальный результат в игре в страны: {max_country_score}",
                      reply_markup=markup)
     rules_button = types.InlineKeyboardButton("Правила ", callback_data='edit')
     start_game_button = types.InlineKeyboardButton("Начать новую игру", callback_data='start_game')
@@ -73,8 +84,10 @@ def choose_game(call):
     markup = types.InlineKeyboardMarkup(row_width=1)
     cities_game_button = types.InlineKeyboardButton("Игра в российские города", callback_data='cities_game')
     words_game_button = types.InlineKeyboardButton("Игра в слова", callback_data='words_game')
-    markup.add(cities_game_button, words_game_button)
-    bot.send_message(call.message.chat.id, "У меня есть две игры: города России и слова, что выберешь?",
+    country_game_button = types.InlineKeyboardButton('Игра в страны', callback_data='countries_game')
+    markup.add(cities_game_button, words_game_button, country_game_button)
+    bot.send_message(call.message.chat.id, "У меня есть три игры: города России, игра в слова и игра в "
+                                           "страны, что выберешь?",
                      reply_markup=markup)
 
 
@@ -104,6 +117,17 @@ def start_words_game(call):
                      reply_markup=markup)
 
 
+@bot.callback_query_handler(func=lambda call: call.data == 'countries_game')
+def start_countries_game(call):
+    global current_game
+    current_game = "countries"
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    finish_button = types.InlineKeyboardButton("Завершить игру", callback_data='finish_game')
+    markup.add(finish_button)
+    bot.send_message(call.message.chat.id, "Давайте начнем игру в страны! Назовите страну на любую букву.",
+                     reply_markup=markup)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == 'finish_game')
 def finish_game(call):
     global used_words
@@ -113,13 +137,13 @@ def finish_game(call):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    global current_game, used_words, word_score, city_score
+    global current_game, used_words, word_score, city_score, country_score
     if current_game == "cities":
         global used_words, max_city_score, city_score
         if len(message.text.split()) != 1:
             bot.send_message(message.chat.id, "Нужно ввести одно слово!")
             return
-        city = message.text.strip().lower().capitalize()  # Приводим город к единому формату
+        city = message.text.strip().lower().title()
         markup = types.InlineKeyboardMarkup(row_width=1)
         finish_button = types.InlineKeyboardButton("Завершить игру", callback_data='finish_game')
         markup.add(finish_button)
@@ -212,6 +236,48 @@ def handle_message(message):
                              reply_markup=markup)
             used_words.append(word)
             word_score += 1
+            return
+    if current_game == 'countries':
+        if len(message.text.split()) != 1:
+            bot.send_message(message.chat.id, "Нужно ввести одно слово!")
+            return
+        word = message.text.lower().title()
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        finish_button = types.InlineKeyboardButton("Завершить игру", callback_data='finish_game')
+        markup.add(finish_button)
+
+        if len(used_words) == 0:
+            if word[-1] in "ьъы":
+                bot.send_message(message.chat.id, f"Отлично! Теперь назови слово, начинающееся на букву "
+                                                  f"'{word[-2].upper()}'.",
+                                 reply_markup=markup)
+
+            if word[-1] not in "ьъы":
+                bot.send_message(message.chat.id, f"Отлично! Теперь назови слово, начинающееся на букву "
+                                                  f"'{word[-1].upper()}'.",
+                                 reply_markup=markup)
+            used_words.append(word)
+            country_score += 1
+            return
+        elif word not in country:
+            bot.send_message(message.chat.id, f"Такой страны нет, назови другую страну.",
+                             reply_markup=markup)
+        elif word in used_words:
+            bot.send_message(message.chat.id, "Эта страна уже была использована. Пожалуйста, введите другую страну.",
+                             reply_markup=markup)
+        elif word[-1] in "ьъы" and word not in used_words:
+            bot.send_message(message.chat.id, f"Отлично! Теперь назови страну, начинающуюся на букву "
+                                              f"'{word[-2].upper()}'.",
+                             reply_markup=markup)
+            used_words.append(word)
+            country_score += 1
+            return
+        elif word[-1] not in "ьъы" and word not in used_words:
+            bot.send_message(message.chat.id, f"Отлично! Теперь назови страну, начинающуюся на букву "
+                                              f"'{word[-1].upper()}'.",
+                             reply_markup=markup)
+            used_words.append(word)
+            country_score += 1
             return
 
 
